d1ddd55bf9aa038c790acb7a10a06500e3864d58
Brie Coyle <brie.coyle@gmail.com>
2016-07-25 16:20:06 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+# a Dog has 4 legs
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+# a Person has 2 legs
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
